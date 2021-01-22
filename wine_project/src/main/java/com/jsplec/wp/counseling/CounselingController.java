616a392714dfacb053ecb3f01f2a1d1feaf87dff
Derrick Kim <59466342+thlbyl0109@users.noreply.github.com>
2021-01-22 14:26:05 +0900

    21.01.22_태현
    delete

--- wine_project/src/main/java/com/jsplec/wp/counseling/CounselingController.java
+++
@@ -1,9 +0,0 @@
-package com.jsplec.wp.counseling;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class CounselingController {
-
-	
-}
--- wine_project/src/main/java/com/jsplec/wp/sub/MainSubDto.java
+++
@@ -1,5 +0,0 @@
-package com.jsplec.wp.sub;
-
-public class MainSubDto {
-
-}
