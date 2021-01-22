46e745690b85a04cc93748a8fa55d1997903b3d7
juhyejeong <wnshddlgpejddl@hanmail.net>
2021-01-22 11:03:42 +0900

    2021.01.22 혜정
    Counseling PKG
    controller,dto,dao,interface 모아놓음

---
+++ wine_project/src/main/java/com/jsplec/wp/counseling/CounselingController.java
@@ -0,0 +1,9 @@
+package com.jsplec.wp.counseling;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CounselingController {
+
+	
+}
--- wine_project/src/main/java/com/jsplec/wp/counseling/CounselingIDao.java
+++ wine_project/src/main/java/com/jsplec/wp/counseling/CounselingIDao.java
@@ -2,16 +2,17 @@ package com.jsplec.wp.counseling;
 
 import java.util.ArrayList;
 
-
-
 public interface CounselingIDao {
 	
-	
-	
+
 	public ArrayList<ContentDto> listDao();
 	public void writeDao(String mWriter, String mContent , String mAddr, String mEmail, String mRelation);
 	public void deleteDao(String string); // 들어가는 변수명 달라도 됨ㅋㅋㅋ 스트링 인트 암상관없긔 ㅎㅋ 지멋대로임ㅋ
 	public ContentDto updateviewDao(String mId);
 	public void updateDao(String mWriter, String mContent , String mAddr, String mEmail, String mRelation ,String mId);
+	
+	
+	
+	
 
 }
